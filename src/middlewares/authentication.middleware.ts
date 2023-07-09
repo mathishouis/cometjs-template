1ab873d409f5c3458997d1ae92cd28aeef8e31f3
Mathis HOUIS <mathishouis@gmail.com>
2023-07-09 18:52:37 +0200

    + Implemented middleware example

--- src/middlewares/authentication.middleware.ts
+++ src/middlewares/authentication.middleware.ts
@@ -1,5 +1,8 @@
-import { Middleware } from "cometjs";
+import { Closure, Middleware, Request } from "cometjs";
 
 export class AuthenticationMiddleware extends Middleware {
-
+  handle(request: Request, next: Closure): void {
+    // Implement auth verification
+    return next(request);
+  }
 }
