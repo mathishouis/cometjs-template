a96c3e33b2cbe97f9823e91bc716d92727e4b425
Mathis HOUIS <mathishouis@gmail.com>
2023-07-10 15:59:16 +0200

    + Implemented TypeORM

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,9 @@
 import { Comet, Router } from "cometjs";
 import { config } from "dotenv";
 import { AdminController, ApiController, HomeController } from "./controllers";
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+import { User } from "./models/user.model";
 
 /**
  * Environment configuration initialization
@@ -8,19 +11,47 @@ import { AdminController, ApiController, HomeController } from "./controllers";
 config();
 
 /**
- *  Route initialization
+ * CometJS initialization
  */
-const router: Router = new Router();
-router.controller(HomeController);
-router.controller(ApiController);
-router.controller(AdminController);
+
+const initialize = (): void => {
+  /**
+   *  Route initialization
+   */
+  const router: Router = new Router();
+  router.controller(HomeController);
+  router.controller(ApiController);
+  router.controller(AdminController);
+
+  /**
+   * Comet initialization
+   */
+  const host: string = process.env.APP_HOST ?? "127.0.0.1";
+  const port: string = process.env.APP_PORT ?? "8080";
+
+  const app: Comet = new Comet()
+    .use(router)
+    .listen(host, Number(port));
+}
 
 /**
- * Comet initialization
+ * Database initialization
  */
-const host: string = process.env.APP_HOST ?? "127.0.0.1";
-const port: string = process.env.APP_PORT ?? "8080";
-
-const app: Comet = new Comet()
-  .use(router)
-  .listen(host, Number(port));
+export const dataSource: DataSource = new DataSource({
+  type: "mysql",
+  host: "127.0.0.1",
+  port: 3306,
+  username: "root",
+  password: "",
+  database: "cometjs",
+  synchronize: true,
+  logging: true,
+  entities: [
+    User,
+  ]
+});
+dataSource.initialize()
+  .then(() => {
+    initialize();
+  })
+  .catch((error) => console.log(error));
---
+++ src/models/user.model.ts
@@ -0,0 +1,18 @@
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class User {
+
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  username: string;
+
+  @Column()
+  email: string;
+
+  @Column()
+  password!: string;
+
+}
